ba4c764bf6acc204787ae59cc32ea13fb4117791
samy-belazzoug <samy.belazzoug@laplateforme.io>
2025-02-03 23:01:47 +0100

    Added if name == main

--- jour01/job02.py
+++ jour01/job02.py
@@ -2,6 +2,7 @@ class Operation:
     def __init__(self,nombre1=1,nombre2=2):
         self.nombre1 = nombre1
         self.nombre2 = nombre2
-if __name__ == "__main__":
-    nombre = Operation(1,2)
-    print(nombre)        
+    
+    def __repr__(self):
+        return f"Le nombre1 est {self.nombre1}\nLe nombre2 est {self.nombre2}"
+
