a85a9bb0bf21f0b1ab7cb6888fafece11bdbe75e
samy-belazzoug <samy.belazzoug@laplateforme.io>
2025-02-03 23:02:52 +0100

    Job 2 Done.

--- jour01/job02.py
+++ jour01/job02.py
@@ -2,7 +2,9 @@ class Operation:
     def __init__(self,nombre1=1,nombre2=2):
         self.nombre1 = nombre1
         self.nombre2 = nombre2
-    
     def __repr__(self):
         return f"Le nombre1 est {self.nombre1}\nLe nombre2 est {self.nombre2}"
 
+if __name__ == "__main__":
+    nombres = Operation(12,3)
+    print(nombres)
