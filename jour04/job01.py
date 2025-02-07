45de7ef1f0eb064b6bf5168968aabe12a5d12589
Samy Belazzoug <samy.belazzoug@laplateforme.io>
2025-02-07 11:36:08 +0100

    Job 1 Done.

---
+++ jour04/job01.py
@@ -0,0 +1,34 @@
+class Personnage:
+    def __init__(self,age:int=14):
+        self.age = age
+    
+    def afficherAge(self):
+        return self.age
+
+    def bonjour():
+        return "Hello"
+    
+    def modifierAge(self,age:int):
+        self.age = age
+
+class Eleve(Personnage):
+    def __init__(self,age=14):
+        super().__init__(age)
+    
+    def allerEnCours(self):
+        return "Je vais en cours"
+    
+    def afficherAge(self):
+        return f"J'ai {self.age} ans"
+
+class Professeur:
+    def __init__(self,matiereEnseignee):
+        self.__matiereEnseignee = matiereEnseignee
+    
+    def enseigner():
+        return "Le cours va commencer"
+
+if __name__ == "__main__":
+    Nico = Personnage()
+    Ulys = Eleve()
+    print(Ulys.afficherAge())
